a5849ca5ee4e7f2465cb5012f75d3e3b3d63e1e8
nwshane <django09@gmail.com>
2017-11-03 19:43:25 -0400

    Implemented reducer that transforms list of strings into a map of emoji counts in the strings.

--- lib/twitter_emojis.ex
+++ lib/twitter_emojis.ex
@@ -1,4 +1,5 @@
 defmodule TwitterEmojis do
+  use Application
   @moduledoc """
   Documentation for TwitterEmojis.
   """
@@ -14,5 +15,30 @@ defmodule TwitterEmojis do
   """
   def hello do
     :world
+  end
+
+  def test_strings do
+    [
+      "yes this is my 😀  favorite emoji!!! 😱😱",
+      "hello world 😀 fdsf",
+      "Albert's feeling like 🍕"
+    ]
+  end
+
+  def parse_string(string, map) do
+    string
+    |> to_charlist
+    |> Enum.reduce(map, fn
+      (char, mapAcc) when char >= 127829 -> Map.update(mapAcc, to_string([char]), 1, &(&1 + 1))
+      (_char, mapAcc) -> mapAcc
+    end)
+  end
+
+  def start(_type, _args) do
+    test_strings()
+    |> Enum.reduce(%{}, &TwitterEmojis.parse_string/2)
+    |> IO.inspect
+
+    Supervisor.start_link [], strategy: :one_for_one
   end
 end
