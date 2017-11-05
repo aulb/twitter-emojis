7e736e35d32aebbc29985ff6b24e293af2872ccf
aulb <aulbert@inside-65-93-208.wireless.utoronto.ca>
2017-11-04 20:03:34 -0400

    Adding notes.

--- lib/twitter_emojis.ex
+++ lib/twitter_emojis.ex
@@ -14,28 +14,45 @@ defmodule TwitterEmojis do
 
   @doc """
   ## Examples
+    iex> TwitterEmojis.is_emoji(?😀)
+    true
 
-      iex> TwitterEmojis.is_emoticon(?😀)
-      true
+    iex> TwitterEmojis.is_emoji(?a)
+    false
 
-      iex> TwitterEmojis.is_emoticon(?a)
-      false
+  Notes:
+  1) Why are we using defmacro?
+  The reason why ... TODO
+
+  2) Be very careful with single quote and double quote
+  Double quote is a string
+  Single quote is a charlist -> is just a list
+
+  3) Source: https://stackoverflow.com/questions/30757193/find-out-if-character-in-string-is-emoji
   """
-  defmacro is_emoticon(char) do
+  defmacro is_emoji(char) do
+    # What is quote? Gets the binary representation of its expression
     quote do: unquote(char) in 0x1F600..0x1F64F or unquote(char) in 0x1F300..0x1F5FF
   end
 
   def parse_string(string, map) do
     string
+    # Convert char string to binary rep
     |> to_charlist
     |> Enum.reduce(map, fn
-      (char, mapAcc) when is_emoticon(char) -> Map.update(mapAcc, to_string([char]), 1, &(&1 + 1))
+      # Read more: The & shorthand, parameters are available as &1 &2 &3 ..., function that accepts one argument
+      # f(x) = x + 1
+      # TODO: Learn more about guards
+      (char, mapAcc) when is_emoji(char) -> Map.update(mapAcc, to_string([char]), 1, &(&1 + 1))
+      # If its not an emoticon, then return the map untouched
       (_char, mapAcc) -> mapAcc
     end)
   end
 
   def start(_type, _args) do
+    # Test strings are being enumerated
     test_strings()
+    # TODO: How come it knows to pass %{} as the second argument?
     |> Enum.reduce(%{}, &TwitterEmojis.parse_string/2)
     |> IO.inspect
 
