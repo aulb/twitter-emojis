0236a6e7df297e3798a94d459c4c81bfe108138b
aulb <aulbert@aulb.local>
2017-11-05 16:27:44 -0500

    No longer guarded

--- lib/twitter_emojis.ex
+++ lib/twitter_emojis.ex
@@ -4,35 +4,7 @@ defmodule TwitterEmojis do
   Documentation for TwitterEmojis.
   """
 
-  @emoji_ranges [
-    [ supplemental_symbols_pictographs: 0x1F900..0x1F9FF ],
-    [ transport_and_map:        0x1F680..0x1F6FF ], 
-    [ misc_pictographs:         0x1F300..0x1F5FF ],
-    [ emoticons:                0x1F600..0x1F64F ],
-    [ dingbats:             0x2700..0x27BF   ], 
-    [ misc_symbols:           0x2600..0x26FF   ],  
-    [ variation_selectors_one:      0xFE00..0xFE0F   ], 
-    [ variation_selectors_two:      65024..65039     ], 
-    [ diactrical_marks_for_symbols:   8400..8447       ] 
-  ]
-
-
-  def test_strings do
-    [
-      "yes this is my 😀  favorite emoji!!! 😱😱",
-      "hello w🌀rld 😀 🗿fdsf",
-      "Albert's feeling like 🙏🍕"
-    ]
-  end
-
   @doc """
-  ## Examples
-    iex> TwitterEmojis.is_emoji(?😀)
-    true
-
-    iex> TwitterEmojis.is_emoji(?a)
-    false
-
   Notes:
   1) Why are we using defmacro?
   The reason why ... TODO
@@ -43,26 +15,37 @@ defmodule TwitterEmojis do
 
   3) Source: https://stackoverflow.com/questions/30757193/find-out-if-character-in-string-is-emoji
   """
-  defmacro is_emoticon(char) do
-    # What is quote?
-    quote do: 
-      # Enum.any?(unquote(emoji_ranges), fn(emoji_range) -> unquote(is_in_range) end)
-      unquote(char) in 0x1F600..0x1F64F or unquote(char) in 0x1F300..0x1F5FF
-      # is_emoji(unquote(char))
+  @emoji_ranges [
+    0x1F900..0x1F9FF,
+    0x1F680..0x1F6FF,
+    0x1F300..0x1F5FF,
+    0x1F600..0x1F64F,
+    0x2700..0x27BF,
+    0x2600..0x26FF,
+    0xFE00..0xFE0F,  
+    65024..65039, 
+    8400..8447
+  ]
+
+  def test_strings do
+    [
+      "yes this is my 😀  favorite emoji!!! 😱😱",
+      "hello w🌀rld 😀 🗿fdsf",
+      "Albert's feeling like 🙏🍕"
+    ]
   end
 
   def parse_string(string, map) do
-    string
     # Convert char string to binary rep
+    string
     |> to_charlist
-    |> Enum.reduce(map, fn
-      # Read more: The & shorthand, parameters are available as &1 &2 &3 ..., function that accepts one argument
-      # f(x) = x + 1
-      # TODO: Learn more about guards
-      (char, mapAcc) when is_emoticon(char) -> Map.update(mapAcc, to_string([char]), 1, &(&1 + 1))
-      # If its not an emoticon, then return the map untouched
-      (_char, mapAcc) -> mapAcc
-    end)
+    |> Enum.filter(fn
+      (char) -> Enum.any?(@emoji_ranges, fn(range) -> char in range end)
+      end)
+    |> Enum.reduce(map, fn(char, mapAcc) ->  
+      Map.update(mapAcc, to_string([char]), 1, &(&1 + 1))
+      end)
+    |> IO.inspect
   end
 
   def start(_type, _args) do
