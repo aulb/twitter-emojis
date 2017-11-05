59960d6d33ae874c259d9b3a0e2dc71d81b9c275
aulb <aulbert@aulb.local>
2017-11-05 15:54:51 -0500

    Revert back to how it was yesterday (working).

--- lib/twitter_emojis.ex
+++ lib/twitter_emojis.ex
@@ -4,6 +4,19 @@ defmodule TwitterEmojis do
   Documentation for TwitterEmojis.
   """
 
+  @emoji_ranges [
+    [ supplemental_symbols_pictographs: 0x1F900..0x1F9FF ],
+    [ transport_and_map:        0x1F680..0x1F6FF ], 
+    [ misc_pictographs:         0x1F300..0x1F5FF ],
+    [ emoticons:                0x1F600..0x1F64F ],
+    [ dingbats:             0x2700..0x27BF   ], 
+    [ misc_symbols:           0x2600..0x26FF   ],  
+    [ variation_selectors_one:      0xFE00..0xFE0F   ], 
+    [ variation_selectors_two:      65024..65039     ], 
+    [ diactrical_marks_for_symbols:   8400..8447       ] 
+  ]
+
+
   def test_strings do
     [
       "yes this is my 😀  favorite emoji!!! 😱😱",
@@ -30,31 +43,15 @@ defmodule TwitterEmojis do
 
   3) Source: https://stackoverflow.com/questions/30757193/find-out-if-character-in-string-is-emoji
   """
-  defmacro is_emoticon(char, emoji_ranges) do
-    is_in_range = quote do
-      char in emoji_range
-    end
-
+  defmacro is_emoticon(char) do
     # What is quote?
     quote do: 
-      Enum.any?(unquote(emoji_ranges), fn(emoji_range) -> unquote(is_in_range) end)
-      # unquote(char) in 0x1F600..0x1F64F or unquote(char) in 0x1F300..0x1F5FF
+      # Enum.any?(unquote(emoji_ranges), fn(emoji_range) -> unquote(is_in_range) end)
+      unquote(char) in 0x1F600..0x1F64F or unquote(char) in 0x1F300..0x1F5FF
       # is_emoji(unquote(char))
   end
 
   def parse_string(string, map) do
-    emoji_ranges = [
-      0x1F600..0x1F64F,  # Emoticons
-      0x1F300..0x1F5FF,  # Misc Symbols and Pictographs
-      0x1F680..0x1F6FF,  # Transport and Map
-      0x2600..0x26FF,    # Misc symbols
-      0x2700..0x27BF,    # Dingbats
-      0xFE00..0xFE0F,    # Variation Selectors
-      0x1F900..0x1F9FF,  # Supplemental Symbols and Pictographs
-      65024..65039,      # Variation selector
-      8400..8447         # Combining Diacritical Marks for Symbols
-    ]
-
     string
     # Convert char string to binary rep
     |> to_charlist
@@ -62,7 +59,7 @@ defmodule TwitterEmojis do
       # Read more: The & shorthand, parameters are available as &1 &2 &3 ..., function that accepts one argument
       # f(x) = x + 1
       # TODO: Learn more about guards
-      (char, mapAcc) when is_emoticon(char, emoji_ranges) -> Map.update(mapAcc, to_string([char]), 1, &(&1 + 1))
+      (char, mapAcc) when is_emoticon(char) -> Map.update(mapAcc, to_string([char]), 1, &(&1 + 1))
       # If its not an emoticon, then return the map untouched
       (_char, mapAcc) -> mapAcc
     end)
